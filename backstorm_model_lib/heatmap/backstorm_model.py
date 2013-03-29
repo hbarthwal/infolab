c0a5b378c8497a598a4a5d930623b3cd1c8a3857
Himanshu Barthwal <shady3025@tamu.edu>
2013-03-29 02:37:40 -0500

    Initial commit for backstorm model implementation

---
+++ backstorm_model_lib/heatmap/backstorm_model.py
@@ -0,0 +1,38 @@
+'''
+Created on Mar 24, 2013
+
+@author: Himanshu Barthwal
+'''
+from region import Region
+from extractdata import DataExtractor
+
+class BackStormModelGenerator:
+    
+    dictExpertiseRegions =  {'tech': Region()}
+
+    
+    def getMaxLikelihoodValue(self, region):
+        print 'getting the max likelihood value for region', region
+        
+   
+    def calculateLogLikelihood(self, region):
+        print 'calculating the log likelihood value for region', region
+        
+    def getMaximumLikelyRegion(self, region):
+        print 'calculating maximum likelihood for all regions and getting the maximum region'
+    
+    def createParentRegions(self):
+        print 'creating parent Regions'
+    
+    def createParentRegion(self, expertise):
+        print 'Creating region for ', expertise 
+    
+    
+    
+    
+    
+    
+                     
+        
+         
+        
